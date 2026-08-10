d1093974110668d45a4ce9a1952f9fce63dff406
baljeesi17 <baljeet.singh171290@gmail.com>
2026-08-11 01:32:16 +0530

    Remove Azure backend configuration from provider.tf
    Removed backend configuration for Azure storage.

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -8,14 +8,6 @@ terraform {
     }
   }
 
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"
-    storage_account_name = "saterrformstatedemo2026" # Ensure this is globally unique when you create it
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
-  }
-}
-
 provider "azurerm" {
   features {}
 }
